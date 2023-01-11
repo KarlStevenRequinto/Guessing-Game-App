3b3e52856a165c919edf5beed2f5b8f0277a701b
KarlStevenRequinto <lerdrobott@gmail.com>
2023-01-11 17:15:57 +0800

    project structure - screens/components folders

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>GUESSING GAME APP</Text>
+      <Text>asasd GAME APP</Text>
       
     </View>
   );
